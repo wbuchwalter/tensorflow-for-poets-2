082a4b91472eb36f7c9d2a9cea1b730277de783d
William Buchwalter <wbuchwalter@gmail.com>
2017-11-22 14:16:26 -0500

    add start script

--- start_training.sh
+++ start_training.sh
@@ -4,7 +4,7 @@ IMAGE_SIZE=224
 ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"
 
 python -m scripts.retrain \
-  --bottleneck_dir=tf_files/bottlenecks \
+  --bottleneck_dir=/tmp/bottlenecks \
   --how_many_training_steps=500 \
   --model_dir=tf_files/models/ \
   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
